dcd10ed4620445b5fac50ee98e654745a6207a46
Kyle Hipolito <kyle.hipolito@quasset.com>
2023-08-23 11:27:45 +0800

    done: problem #2

--- 2.go
+++ 2.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func problem2 () {
+  sum := 0
 
+  prev := 1
+  cur := 2
+
+  for cur < 4*1e6 {
+    if cur%2 == 0 {
+      sum += cur
+    }
+
+    next := prev + cur
+
+    prev = cur
+    cur = next
+  }
+
+  fmt.Println(sum)
 }
